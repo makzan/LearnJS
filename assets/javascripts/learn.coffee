fffd919118b08142eabeb29b26b734e98076e29b
Thomas Seng Hin Mak <makzan@gmail.com>
2012-05-03 10:39:58 +0800

    Looping the slides and wigets now to render all content

--- assets/javascripts/learn.coffee
+++ assets/javascripts/learn.coffee
@@ -1,15 +1,38 @@
+
+source = $('#source')
+output = $('#output')
+storage = null
+
 $ ->
   console.log 'Welcome to LearnJS console.'
-  source = $('#source');
   
-  source.val localStorage['source']
+  
+  storage = new Storage()
+  storage.load()
+  
+  renderer = new Renderer source
+  
+  renderer.render()
   
   source.change ->
+    console.log "Changes"
+    renderer.render()
+
+class Storage
+  load: ->
+    source.val localStorage['source']
+  save: (str) ->
+    localStorage['source'] = str
+
+class Renderer
+  
+  constructor: (@source) -> yes
+  render: ->
     # get the source code
     str = source.val()
     
     # save the document
-    localStorage['source'] = str
+    storage.save str
     
     # parse the yaml into JS object
     obj = jsyaml.load(str)
@@ -19,18 +42,50 @@ $ ->
     console.log obj
     console.log JSON.stringify obj
     
-    content = obj.slides[0].content
+    output.empty()
     
-    # the markdown parser
-    converter = new Showdown.converter();
+    for slide in obj.slides
+         for widget in slide.widgets
+           switch widget.type
+             when 'text'
+               content = widget.content
+         
+               # the markdown parser
+               converter = new Showdown.converter();
+       
+               # and parse the content into HTML
+               content = converter.makeHtml(content);                              
+               output.append "<div>#{content}</div>"
+             when 'image'
+               output.append "<img src='#{widget.src}'>"
+               
+            
+      #   
+      # 
+      # try 
+      #   content = obj.slides[0].widgets[0].content
+      #   
+      #   # the markdown parser
+      #   converter = new Showdown.converter();
+      # 
+      #   # and parse the content into HTML
+      #   content = converter.makeHtml(content);
+      #   
+      # catch error
+      #   console.log "Found error: #{error}"
+      #   
+      # 
+      # 
+      # 
+      # 
+      # 
+      # try 
+      #   content2 = obj.slides[0].widgets[1]
+      #   output.append "<img src='#{content2.src}'>"
+      # catch error
+      #   console.log "Found error: #{error}"
+      
     
-    # and parse the content into HTML
-    content = converter.makeHtml(content);
     
-    $('#test').remove()
-    $('body').append "<div id='test'></div>"
-    $('#test').append content
-    
-    content2 = obj.slides[1]
-    $('#test').append "<img src='#{content2.src}'>"
+	
     
--- assets/javascripts/learn.js
+++ assets/javascripts/learn.js
@@ -1,27 +1,89 @@
 (function() {
+  var Renderer, Storage, output, source, storage;
+
+  source = $('#source');
+
+  output = $('#output');
+
+  storage = null;
 
   $(function() {
-    var source;
+    var renderer;
     console.log('Welcome to LearnJS console.');
-    source = $('#source');
-    source.val(localStorage['source']);
+    storage = new Storage();
+    storage.load();
+    renderer = new Renderer(source);
+    renderer.render();
     return source.change(function() {
-      var content, content2, converter, obj, str;
+      console.log("Changes");
+      return renderer.render();
+    });
+  });
+
+  Storage = (function() {
+
+    function Storage() {}
+
+    Storage.prototype.load = function() {
+      return source.val(localStorage['source']);
+    };
+
+    Storage.prototype.save = function(str) {
+      return localStorage['source'] = str;
+    };
+
+    return Storage;
+
+  })();
+
+  Renderer = (function() {
+
+    function Renderer(source) {
+      this.source = source;
+      true;
+    }
+
+    Renderer.prototype.render = function() {
+      var content, converter, obj, slide, str, widget, _i, _len, _ref, _results;
       str = source.val();
-      localStorage['source'] = str;
+      storage.save(str);
       obj = jsyaml.load(str);
       console.log("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
       console.log(obj);
       console.log(JSON.stringify(obj));
-      content = obj.slides[0].content;
-      converter = new Showdown.converter();
-      content = converter.makeHtml(content);
-      $('#test').remove();
-      $('body').append("<div id='test'></div>");
-      $('#test').append(content);
-      content2 = obj.slides[1];
-      return $('#test').append("<img src='" + content2.src + "'>");
-    });
-  });
+      output.empty();
+      _ref = obj.slides;
+      _results = [];
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        slide = _ref[_i];
+        _results.push((function() {
+          var _j, _len2, _ref2, _results2;
+          _ref2 = slide.widgets;
+          _results2 = [];
+          for (_j = 0, _len2 = _ref2.length; _j < _len2; _j++) {
+            widget = _ref2[_j];
+            switch (widget.type) {
+              case 'text':
+                content = widget.content;
+                converter = new Showdown.converter();
+                content = converter.makeHtml(content);
+                _results2.push(output.append("<div>" + content + "</div>"));
+                break;
+              case 'image':
+                _results2.push(output.append("<img src='" + widget.src + "'>"));
+                break;
+              default:
+                _results2.push(void 0);
+            }
+          }
+          return _results2;
+        })());
+      }
+      return _results;
+    };
+
+    return Renderer;
+
+  })();
 
 }).call(this);
